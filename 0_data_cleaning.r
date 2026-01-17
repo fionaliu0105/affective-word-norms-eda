c368eb85b7e7913a2ec1f35d212ab7791bec08b2
fionaliu0105 <fionakql@gmail.com>
2026-01-17 00:23:08 -0600

    updated code to final version

--- 0_data_cleaning.r
+++ 0_data_cleaning.r
@@ -1,9 +1,29 @@
-# every script should be self inclusive
+
 library(tidyverse)
 library(knitr)
+library(gt)
+library(patchwork)
 
 affective_data <- read_csv("data/BRM-emot-submit.csv")
 
-# data cleaning
-# variable typing etc. 
 
+summary_stats <- affective_data |>
+  summarise(
+    Variable = c("Valence", "Arousal", "Dominance"),
+    Mean = c(mean(V.Mean.Sum), mean(A.Mean.Sum), mean(D.Mean.Sum)),
+    Median = c(median(V.Mean.Sum), median(A.Mean.Sum), median(D.Mean.Sum)),
+    SD = c(sd(V.Mean.Sum), sd(A.Mean.Sum), sd(D.Mean.Sum)),
+    Min = c(min(V.Mean.Sum), min(A.Mean.Sum), min(D.Mean.Sum)),
+    Max = c(max(V.Mean.Sum), max(A.Mean.Sum), max(D.Mean.Sum))
+  )
+
+summary_stats |>
+  gt() |>
+  fmt_number(columns = c(Mean, Median, SD), decimals = 2) |>
+  cols_label(SD = "Std. Dev.") |>
+  tab_header(title = "Descriptive Statistics of Affective Dimensions") |>
+  opt_align_table_header(align = "left") |>
+  tab_style(
+    style = cell_text(weight = "bold"),
+    locations = cells_column_labels()
+  )
